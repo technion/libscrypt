f715c50d45fe76e4e01dd26b5c8f8855a876d11c
Technion <technion@lolware.net>
2013-06-04 00:30:06 -0400

    Implemented secure compile flags. Implemented workaround for issues when D_FORITFY_SOURCE=2

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=gcc
-CFLAGS=-O2 -Wall -g 
+CFLAGS=-O2 -Wall -g -D_FORTIFY_SOURCE=2 -fstack-protector
 all: reference
 
 OBJS= crypto_scrypt-nosse.o sha256.o crypto_scrypt-hexconvert.o crypto-mcf.o modp_b64.o crypto-scrypt-saltgen.o crypto_scrypt-check.o crypto_scrypt-hash.o
--- crypto_scrypt-hexconvert.c
+++ crypto_scrypt-hexconvert.c
@@ -7,16 +7,17 @@ int crypto_scrypt_hexconvert(uint8_t *buf, size_t s, char *outbuf, size_t obs)
 {
 
         int i;
+	int len = 0;
 
         if (!buf || s < 1 || obs < (s * 2 + 1))
                 return 0;
 
-         memset(outbuf, 0, obs);
+        memset(outbuf, 0, obs);
 	
 
         for(i=0; i<=(s-1); i++)
         {
-                sprintf(outbuf, "%s%02x", outbuf, (unsigned char) buf[i]);
+                len += sprintf(outbuf+len, "%02x", (unsigned char) buf[i]);
         }
 
 	return 1;
