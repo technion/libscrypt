6abf9d1ec935d5a1f5b22b1f0088fd17242affb6
Jan Varho <jan@varho.org>
2014-05-08 13:25:44 +0300

    Remove unexported libscrypt_hexconvert from libscrypt.h

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 #include "b64.h"
+#include "crypto_scrypt-hexconvert.h"
 #include "libscrypt.h"
 
 #define REF1 "fdbabe1c9d3472007856e7190d01e9fe7c6ad7cbc8237830e77376634b3731622eaf30d92e22a3886ff109279d9830dac727afb94a83ee6d8360cbdfa2cc0640"
